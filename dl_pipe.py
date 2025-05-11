459cb2232bd40798d0b208766fc8f6f75df475d2
to <tuvotg@gmail.com>
2025-05-11 13:55:59 -0400

    Added Batch Inference

--- dl_pipe.py
+++ dl_pipe.py
@@ -6,12 +6,12 @@ import numpy as np
 import logging
 import cv2
 import os
+import time
 
 from onnx import helper
 from google.protobuf.json_format import MessageToDict
 
 IN_VIDEO_PATH = "./assets/highlight.webm"
-OUT_VIDEO_PATH = "output.webm"
 MODEL_PATH = "./best.onnx"
 
 logger = logging.getLogger(__name__)
@@ -20,6 +20,8 @@ logging.basicConfig(level=logging.INFO)
 def load_onnx():
     onnx_model = onnx.load(MODEL_PATH)
     onnx.checker.check_model(onnx_model)
+    
+    logger.info(f'Available EPs: {ort.get_available_providers()}')
     
     for _input in onnx_model.graph.input:
         elem_type = _input.type.tensor_type.elem_type
@@ -67,17 +69,6 @@ def start_ffmpeg_process1(in_filename):
     )
     return subprocess.Popen(args, stdout=subprocess.PIPE)
 
-def start_ffmpeg_process2(out_filename, width, height):
-    logger.info('Starting ffmpeg process2')
-    args = (
-        ffmpeg
-        .input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height))
-        .output(out_filename, pix_fmt='yuv420p')
-        .overwrite_output()
-        .compile()
-    )
-    return subprocess.Popen(args, stdin=subprocess.PIPE)
-
 def read_frame(process1, width, height, input_size=(640, 640)):
     logger.debug('Reading frame')
     frame_size = width * height * 3
@@ -97,6 +88,29 @@ def read_frame(process1, width, height, input_size=(640, 640)):
         resized = cv2.resize(full_res, input_size, interpolation=cv2.INTER_AREA)
         tensor = resized.astype(np.float32) / 255.0
         model_input = np.transpose(tensor, (2,0,1))[None, ...]
+
+    return full_res, model_input
+
+def read_frame_batch(process1, width, height, input_size=(640, 640)):
+    logger.debug('Reading frame')
+    frame_size = width * height * 3
+    in_bytes = process1.stdout.read(frame_size)
+    if len(in_bytes) == 0:
+        return None, None
+    else:
+        assert len(in_bytes) == frame_size
+        
+        full_res = (
+            np
+            .frombuffer(in_bytes, np.uint8)
+            .reshape((height, width, 3))  
+            .copy()
+        )
+        
+        resized = cv2.resize(full_res, input_size, interpolation=cv2.INTER_AREA)
+        tensor = resized.astype(np.float32) / 255.0
+        model_input = np.transpose(tensor, (2,0,1))
+        
 
     return full_res, model_input
 
@@ -128,30 +142,48 @@ def process_frame(full_res, model_input, session, conf_threshold=0.9):
 
     return full_res
     
-def write_frame(process2, frame):
-    logger.debug('Writing frame')
-    process2.stdin.write(
-        frame
-        .astype(np.uint8)
-        .tobytes()
-    )
-
-def run(in_filename, out_filename, process_frame):
+def process_frame_batch(batch_frames, batch_inputs, session, conf_threshold=0.9):
+    input_name = session.get_inputs()[0].name
+    batch_inputs_stacked = np.stack(batch_inputs, axis=0)
+    raw_output = session.run(None, {input_name: batch_inputs_stacked})[0]
     
+    annotated_batch = []
+    for i, full_res in enumerate(batch_frames):
+        boxes5 = raw_output[i].transpose(1,0)
+        h_full, w_full = full_res.shape[:2]
+        scale_x, scale_y = w_full / 640.0, h_full / 640.0
+
+        for x_c, y_c, bw, bh, conf in boxes5:
+            if conf < conf_threshold:
+                continue
+
+            x1 = x_c - bw / 2
+            y1 = y_c - bh / 2
+            x2 = x_c + bw / 2
+            y2 = y_c + bh / 2
+
+            pt1 = (int(x1 * scale_x), int(y1 * scale_y))
+            pt2 = (int(x2 * scale_x), int(y2 * scale_y))
+
+            cv2.rectangle(full_res, pt1, pt2, (0, 255, 0), 2)
+            label = f"{conf:.2f}"
+            cv2.putText(full_res, label, (pt1[0], pt1[1] - 8),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+
+        annotated_batch.append(full_res)
+
+    return annotated_batch
+
+def write_frame(annotated, folder, frame_idx):
+    annotated = cv2.cvtColor(annotated, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(f'./debug/{folder}/debug_frame{frame_idx:03d}.png', annotated)
+
+def run(in_filename, session):
     width, height = get_video_size(in_filename)
     process1 = start_ffmpeg_process1(in_filename)
-    # process2 = start_ffmpeg_process2(out_filename, width, height)
-    
-    load_onnx()
-    options = ort.SessionOptions()
-    options.enable_profiling=True
-    session = ort.InferenceSession(
-            MODEL_PATH,
-            sess_options=options,
-            providers=['CUDAExecutionProvider', 'CPUExecutionProvider']
-    )
 
     os.makedirs('./debug', exist_ok=True)    
+    os.makedirs('./debug/single', exist_ok=True)
     frame_idx = 0
     while True:
         full_res, model_input = read_frame(process1, width, height)
@@ -161,20 +193,73 @@ def run(in_filename, out_filename, process_frame):
 
         logger.debug('Processing frame')
         annotated = process_frame(full_res, model_input, session)
-        
-        # write_frame(process2, annotated)
-        annotated = cv2.cvtColor(annotated, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(f'./debug/debug_frame{frame_idx:03d}.png', annotated)
+
+        write_frame(annotated, "single", frame_idx)
         frame_idx += 1
 
     logger.info('Waiting for ffmpeg process1')
     process1.wait()
+    logger.info('Done')
+    
+def run_batch(in_filename, session, batch_size):
+    width, height = get_video_size(in_filename)
+    process1 = start_ffmpeg_process1(in_filename)
+    
+    os.makedirs('./debug', exist_ok=True)    
+    os.makedirs('./debug/batch', exist_ok=True)
+    frame_idx = 0
+    batch_frames, batch_inputs = [], []
+    while True:
+        full_res, model_input = read_frame_batch(process1, width, height)
+        if full_res is None:
+            logger.info('End of input stream')
+            break
 
-    # logger.info('Waiting for ffmpeg process2')
-    # process2.stdin.close()
-    # process2.wait()
+        batch_frames.append(full_res)
+        batch_inputs.append(model_input)
 
+        if len(batch_inputs) == batch_size:
+            logger.debug('Processing batch')
+            annotated_batch = process_frame_batch(batch_frames, batch_inputs, session)
+            batch_frames, batch_inputs = [], []
+
+            for _, annotated in enumerate(annotated_batch):
+                write_frame(annotated, "batch", frame_idx)
+                frame_idx += 1
+
+    if len(batch_frames) != 0:
+            logger.debug('Processing batch')
+            annotated_batch = process_frame_batch(batch_frames, batch_inputs, session)
+            for _, annotated in enumerate(annotated_batch):
+                write_frame(annotated,"batch", frame_idx)
+                frame_idx += 1
+    
+    logger.info('Waiting for ffmpeg process1')
+    process1.wait()
     logger.info('Done')
 
+def load_session():
+    load_onnx()
+    options = ort.SessionOptions()
+    options.enable_profiling=True
+    session = ort.InferenceSession(
+            MODEL_PATH,
+            sess_options=options,
+            providers=['CUDAExecutionProvider', 'CPUExecutionProvider']
+    )
+
+    return session
+
 if __name__ == "__main__":
-    run(IN_VIDEO_PATH, OUT_VIDEO_PATH, process_frame)
+    session = load_session()
+
+    start_single = time.time()
+    run(IN_VIDEO_PATH, session)
+    end_single = time.time()
+    
+    start_batch = time.time()
+    run_batch(IN_VIDEO_PATH, session, batch_size=2)
+    end_batch = time.time()
+    
+    print(f'Single Inference: {end_single-start_single:03f}')
+    print(f'Batch Inference: {end_batch-start_batch:03f}')
